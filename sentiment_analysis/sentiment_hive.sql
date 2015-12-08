a07c7ed3d7a4b0615db70f93a7ea8554d0ee24c0
Miki Seltzer - AMI3 <miki.seltzer@berkeley.edu>
2015-12-08 19:07:00 +0000

    Create script to put sentiment data into Hive

---
+++ sentiment_analysis/sentiment_hive.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS raw_tweets_sentiment;
+DROP TABLE IF EXISTS tweets_sentiment;
+
+CREATE EXTERNAL TABLE raw_tweets_sentiment
+(json STRING)
+
+STORED AS TEXTFILE
+LOCATION '/user/w205/final_proj/raw_tweets_sentiment';
+
+LOAD DATA LOCAL INPATH 'tweet-sentiment_sample.log'
+OVERWRITE INTO TABLE raw_tweets_sentiment;
+
+CREATE TABLE tweets_sentiment AS
+SELECT
+get_json_object(json, "$.text") as text,
+get_json_object(json, "$.date") as created_at,
+unix_timestamp(get_json_object(json, "$.date"), "EEE MMM d HH:mm:ss Z yyyy") as ts_created,
+get_json_object(json, "$.score") as score
+FROM raw_tweets_sentiment;
